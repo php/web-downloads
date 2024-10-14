3ae466e62807b8819403af17b02b0cd7c2cd2212
Shivam Mathur <shivam_jpr@hotmail.com>
2024-10-15 02:11:35 +0530

    Add redirect for uppercase pecl builds in redirect.php

--- public/redirect.php
+++ public/redirect.php
@@ -3,6 +3,12 @@ if (preg_match('/Win32-vc/', $_SERVER['REQUEST_URI'])) {
     $fixed = str_replace( 'Win32-vc', 'Win32-VC', $_SERVER['REQUEST_URI'] );
     header("Location: $fixed", true, 301);
     exit();
+} else if(preg_match('/\/pecl\/.*\/(\d+.*(rc|RC).*\/)php_[^\-]+-([^\-]+-)/', $_SERVER['REQUEST_URI'])) {
+    preg_match_all('/\/pecl\/.*\/(\d+.*(rc|RC).*\/)php_[^\-]+-([^\-]+-)/', $_SERVER['REQUEST_URI'], $matches);
+    $fixed = str_replace($matches[1][0], strtoupper($matches[1][0]), $_SERVER['REQUEST_URI']);
+    $fixed = str_replace($matches[2][0], strtolower($matches[2][0]), $fixed);
+    header("Location: $fixed", true, 301);
+    exit();
 }
 
 header('Location: /', true, 404);
