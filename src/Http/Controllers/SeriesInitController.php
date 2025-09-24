f16126fed01dcdacb47dacc328006583f6daf854
Shivam Mathur <shivam_jpr@hotmail.com>
2025-09-24 19:14:34 +0530

    Add API to init series files

---
+++ src/Http/Controllers/SeriesInitController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\BaseController;
+use App\Validator;
+
+class SeriesInitController extends BaseController
+{
+    protected function validate(array $data): bool
+    {
+        $validator = new Validator([
+            'series' => 'required|string',
+            'series_vs' => 'required|string|regex:/^v[c|s]\d{2}$/',
+            'target_vs' => 'required|string|regex:/^v[c|s]\d{2}$/',
+            'token' => 'required|string',
+        ]);
+
+        $validator->validate($data);
+
+        $valid = $validator->isValid();
+
+        if (!$valid) {
+            http_response_code(400);
+            echo 'Invalid request: ' . $validator;
+        }
+
+        return $valid;
+    }
+
+    protected function execute(array $data): void
+    {
+        extract($data);
+        $directory = getenv('BUILDS_DIRECTORY') . '/series';
+        $hash = hash('sha256', $series) . strtotime('now');
+        $file = $directory . '/series-init-' . $hash . '.json';
+        file_put_contents($file, json_encode($data));
+    }
+}
